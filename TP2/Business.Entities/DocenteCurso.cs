bae9fadcfc0cd593a00f6715e23c35b1d3171ac0
Alexis Quintero <alexisquintero@hotmail.com.ar>
2016-08-25 21:48:53 -0300

    Agregado capa datos docenteCurso

--- TP2/Business.Entities/DocenteCurso.cs
+++ TP2/Business.Entities/DocenteCurso.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Business.Entities
 {
-    class DocenteCurso : BusinessEntity
+    public class DocenteCurso : BusinessEntity
     {
         private int _IDCurso;
         public int IDCurso
