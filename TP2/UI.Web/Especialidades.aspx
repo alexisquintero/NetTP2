f51985eb12f1b1e9d1343ad5e7e717c266d1bf61
Alexis Quintero <alexisquintero@hotmail.com.ar>
2016-10-13 15:50:02 -0300

    Agregado ABM Web de Especialidad

---
+++ TP2/UI.Web/Especialidades.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="Especialidades" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Especialidades.aspx.cs" Inherits="UI.Web.Especialidades" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="bodyContentPlaceHolder" runat="server">
+    <asp:Panel ID="gridPanel" runat="server">
+        <asp:GridView ID="gridView" runat="server" AutoGenerateColumns="False" SelectedRowStyle-BackColor="Black" SelectedRowStyle-ForeColor="White" DataKeyNames="ID" OnSelectedIndexChanged="gridView_SelectedIndexChanged">
+            <Columns>
+                <asp:BoundField DataField="Descripcion" HeaderText="Descripción" />
+                <asp:CommandField SelectText="Seleccionar" ShowSelectButton="true" />
+            </Columns>
+        </asp:GridView>
+    </asp:Panel>
+    <asp:Panel ID="formPanel" runat="server" Visible="False">
+        <asp:Label ID="descripcionLabel" runat="server" Text="Descripción:"></asp:Label>
+        <asp:TextBox ID="descripcionTextBox" runat="server"></asp:TextBox>
+    </asp:Panel>
+    <asp:Panel ID="gridActiconsPanel" runat="server">
+        <asp:LinkButton ID="editarLinkButton" runat="server" OnClick="editarLinkButton_Click">Editar</asp:LinkButton>
+        <asp:LinkButton ID="eliminarLinkButton" runat="server" OnClick="eliminarLinkButton_Click">Eliminar</asp:LinkButton>
+        <asp:LinkButton ID="nuevoLinkButton" runat="server" OnClick="nuevoLinkButton_Click">Nuevo</asp:LinkButton>
+    </asp:Panel>
+    <asp:Panel ID="formActionsPanel" runat="server">
+        <asp:LinkButton ID="aceptarLinkButton" runat="server" OnClick="aceptarLinkButton_Click">Aceptar</asp:LinkButton>
+        <asp:LinkButton ID="cancelarLinkButton" runat="server" OnClick="cancelarLinkButton_Click">Cancelar</asp:LinkButton>
+    </asp:Panel>
+</asp:Content>
---
+++ TP2/UI.Web/Especialidades.aspx.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Web
+{
+    public partial class Especialidades : ApplicationForm
+    {
+        protected new void Page_Load(object sender, EventArgs e)
+        {
+            LoadGrid();
+        }
+        EspecialidadLogic _logic;
+        private EspecialidadLogic Logic
+        {
+            get
+            {
+                if (_logic == null)
+                {
+                    _logic = new EspecialidadLogic();
+                }
+                return _logic;
+            }
+        }
+        private void LoadGrid()
+        {
+            this.gridView.DataSource = this.Logic.GetAll();
+            this.gridView.DataBind();
+        }
+        public Especialidad Entity { get; set; }
+
+        protected void gridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedID = (int)gridView.SelectedValue;
+        }
+        private void LoadForm(int id)
+        {
+            this.Entity = this.Logic.GetOne(id);
+            this.descripcionTextBox.Text = this.Entity.Descripcion;
+            
+        }
+
+        protected void editarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Modificacion;
+                this.LoadForm(this.SelectedID);
+                this.EnableForm(true);
+            }
+        }
+        private void EnableForm(bool enable)
+        {
+            this.descripcionTextBox.Enabled = enable;            
+        }
+        private void LoadEntity(Especialidad especialidad)
+        {
+            especialidad.Descripcion = this.descripcionTextBox.Text;
+        }
+        private void SaveEntity(Especialidad especialidad)
+        {
+            this.Logic.Save(especialidad);
+        }
+
+        protected void aceptarLinkButton_Click(object sender, EventArgs e)
+        {
+            switch (this.FormMode)
+            {
+                case FormModes.Baja:
+                    this.DeleteEntity(this.SelectedID);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Modificacion:
+                    this.Entity = new Especialidad();
+                    this.Entity.ID = this.SelectedID;
+                    this.Entity.State = BusinessEntity.States.Modified;
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Alta:
+                    this.Entity = new Especialidad();
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                default:
+                    break;
+            }
+            this.formPanel.Visible = false;
+        }
+        private void DeleteEntity(int id)
+        {
+            this.Logic.Delete(id);
+        }
+
+        protected void eliminarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Baja;
+                this.EnableForm(false);
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        protected void nuevoLinkButton_Click(object sender, EventArgs e)
+        {
+            this.formPanel.Visible = true;
+            this.FormMode = FormModes.Alta;
+            this.ClearForm();
+            this.EnableForm(true);
+        }
+        private void ClearForm()
+        {
+            this.descripcionTextBox.Text = string.Empty;           
+        }
+
+        protected void cancelarLinkButton_Click(object sender, EventArgs e)
+        {
+            this.LoadGrid();
+            this.formPanel.Visible = false;
+            //TODO: resetear seleccionado en grilla
+        }
+    }
+}
